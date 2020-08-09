0f55feeb36bd8dbf162e9376922ba3818bc63b4a
Keiho Sakapon <git@saka-pon.net>
2020-08-09 16:03:13 +0900

    Add test

--- OperatorsSample/UnitTest/Structs/ValueTupleTest.cs
+++ OperatorsSample/UnitTest/Structs/ValueTupleTest.cs
@@ -2,38 +2,39 @@
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using OperatorsLib.Structs;
 
 namespace UnitTest.Structs
 {
 	[TestClass]
-	public class Vector0Test
+	public class ValueTupleTest
 	{
 		[TestMethod]
 		public void Equals()
 		{
-			var v1 = new Vector0(3, 4);
-			var v2 = new Vector0(3, 4);
-			var v3 = new Vector0(4, 3);
+			var v1 = (3, 4);
+			var v2 = (3, 4);
+			var v3 = (4, 3);
 
 			Assert.IsFalse(ReferenceEquals(v1, v1));
 			Assert.IsTrue(Equals(v1, v1));
 			Assert.IsTrue(v1.Equals(v1));
-			// == 演算子は利用できません。
+			Assert.IsTrue(v1 == v1);
 
 			Assert.IsFalse(ReferenceEquals(v1, v2));
 			Assert.IsTrue(Equals(v1, v2));
 			Assert.IsTrue(v1.Equals(v2));
+			Assert.IsTrue(v1 == v2);
 
 			Assert.IsFalse(ReferenceEquals(v1, v3));
 			Assert.IsFalse(Equals(v1, v3));
 			Assert.IsFalse(v1.Equals(v3));
+			Assert.IsFalse(v1 == v3);
 		}
 
 		[TestMethod]
 		public void Distinct()
 		{
-			var vs = Array.ConvertAll(new int[1000], _ => new Vector0(3, 4));
+			var vs = Array.ConvertAll(new int[1000], _ => (3, 4));
 			Assert.AreEqual(1, vs.Distinct().Count());
 			Assert.AreEqual(1, vs.ToHashSet().Count);
 		}
