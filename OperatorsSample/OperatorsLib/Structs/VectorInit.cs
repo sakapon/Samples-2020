81033307c7f0aa819da83baee8f374fe9354f779
Keiho Sakapon <git@saka-pon.net>
2020-08-15 00:18:52 +0900

    Add samples for equality

---
+++ OperatorsSample/OperatorsLib/Structs/VectorInit.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace OperatorsLib.Structs
+{
+	public struct VectorInit
+	{
+		public double X { get; }
+		public double Y { get; }
+
+		public double Norm { get; }
+		public double Angle { get; }
+
+		// ctor() と ctor(0, 0) の結果が同等になる必要があります。
+		public VectorInit(double x, double y)
+		{
+			(X, Y) = (x, y);
+			Norm = Math.Sqrt(X * X + Y * Y);
+			Angle = Math.Atan2(Y, X);
+		}
+	}
+}
---
+++ OperatorsSample/UnitTest/Classes/VectorEqTest.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OperatorsLib.Classes;
+
+namespace UnitTest.Classes
+{
+	[TestClass]
+	public class VectorEqTest
+	{
+		[TestMethod]
+		public void Equals()
+		{
+			// == 演算子は定義されていませんが、Equals で各要素を比較できます。
+			var v1 = new VectorEq(3, 4);
+			var v2 = new VectorEq(3, 4);
+			var v3 = new VectorEq(4, 3);
+
+			Assert.IsTrue(ReferenceEquals(v1, v1));
+			Assert.IsTrue(Equals(v1, v1));
+			Assert.IsTrue(v1.Equals(v1));
+			// ReferenceEquals と同じです。
+			Assert.IsTrue(v1 == v1);
+
+			Assert.IsFalse(ReferenceEquals(v1, v2));
+			Assert.IsTrue(Equals(v1, v2));
+			Assert.IsTrue(v1.Equals(v2));
+			Assert.IsFalse(v1 == v2);
+
+			Assert.IsFalse(ReferenceEquals(v1, v3));
+			Assert.IsFalse(Equals(v1, v3));
+			Assert.IsFalse(v1.Equals(v3));
+			Assert.IsFalse(v1 == v3);
+		}
+
+		[TestMethod]
+		public void Distinct()
+		{
+			var vs = Array.ConvertAll(new int[1000], _ => new VectorEq(3, 4));
+			Assert.AreEqual(0, Array.IndexOf(vs, new VectorEq(3, 4)));
+			Assert.AreEqual(1, vs.Distinct().Count());
+			Assert.AreEqual(1, vs.ToHashSet().Count);
+		}
+
+		[TestMethod]
+		[ExpectedException(typeof(InvalidOperationException))]
+		public void Sort()
+		{
+			var titles = new[]
+			{
+				new VectorEq(3, 4),
+				new VectorEq(3, 4),
+			};
+
+			Array.Sort(titles);
+		}
+	}
+}
