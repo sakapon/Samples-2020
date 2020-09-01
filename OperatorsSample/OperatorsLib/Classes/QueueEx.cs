d39bc31d3ed141a7da77ff8d11f4360ee966aeae
Keiho Sakapon <git@saka-pon.net>
2020-09-01 22:29:58 +0900

    Add extension methods for Queue<T>

---
+++ OperatorsSample/OperatorsLib/Classes/QueueEx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OperatorsLib.Classes
+{
+	public static class QueueEx
+	{
+		// コレクション初期化子の例。
+		// IEnumerable インターフェイスを実装している型が対象です。
+		// 引数の型に制限はありません。
+		public static void Add<T>(this Queue<T> q, int i)
+		{
+		}
+
+		public static void Add<T>(this Queue<T> q, int i, double d)
+		{
+		}
+
+		public static void Add<T>(this Queue<T> q, int i, string s)
+		{
+		}
+
+		public static void Add<T>(this Queue<T> q, int i, double d, string s)
+		{
+		}
+	}
+}
