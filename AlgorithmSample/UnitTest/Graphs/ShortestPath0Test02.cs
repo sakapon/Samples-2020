a09a248078558e96b7636381fb7582c07e05953f
Keiho Sakapon <git@saka-pon.net>
2020-11-09 15:50:00 +0900

    Add test

---
+++ AlgorithmSample/UnitTest/Graphs/ShortestPath0Test02.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using AlgorithmLab.Graphs;
+
+namespace UnitTest.Graphs
+{
+	// Test: https://codeforces.com/contest/20/problem/C
+	class ShortestPath0Test02
+	{
+		static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+		static void Main()
+		{
+			var h = Read();
+			int n = h[0], m = h[1];
+			var es = Array.ConvertAll(new bool[m], _ => Read());
+
+			var (c, inEdges) = ShortestPath0.Dijkstra(n + 1, es, false, 1, n);
+			if (inEdges[n] == null) { Console.WriteLine(-1); return; }
+
+			var path = ShortestPath0.GetPathVertexes(inEdges, n);
+			Console.WriteLine(string.Join(" ", path));
+		}
+	}
+}
