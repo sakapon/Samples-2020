8886c69d6934706ad5e7331f3ef0987373d18e0d
Keiho Sakapon <git@saka-pon.net>
2021-08-13 12:03:43 +0900

    Add class for PQ using BST

---
+++ AlgorithmSample/AlgorithmLab/Collections/BstPriorityQueue.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AlgorithmLab.Collections
+{
+	// キーが重複しない (すべての値の順序が異なる) 場合に使えます。
+	// First: Min
+	// Push: Add
+	public class DistinctPriorityQueue<T> : SortedSet<T>
+	{
+		public DistinctPriorityQueue() { }
+		public DistinctPriorityQueue(IComparer<T> comparer) : base(comparer) { }
+
+		public T Pop()
+		{
+			var r = Min;
+			Remove(r);
+			return r;
+		}
+	}
+}
