29a2434263b633ce238d1e0f4008d6493a036cc3
Keiho Sakapon <git@saka-pon.net>
2021-12-21 11:24:52 +0900

    Refactor

--- AlgorithmSample/AlgorithmLab/Collections/ComparerHelper.cs
+++ AlgorithmSample/AlgorithmLab/Collections/ComparerHelper.cs
@@ -3,26 +3,36 @@ using System.Collections.Generic;
 
 namespace AlgorithmLab.Collections
 {
+	public static class ComparerHelper
+	{
+		public static IComparer<T> GetDefault<T>()
+		{
+			// カルチャに依存しない場合に高速化します。
+			if (typeof(T) == typeof(string)) return (IComparer<T>)StringComparer.Ordinal;
+			return Comparer<T>.Default;
+		}
+
+		public static IComparer<T> ToDescending<T>(this IComparer<T> c)
+		{
+			if (c == null) throw new ArgumentNullException(nameof(c));
+			return Comparer<T>.Create((x, y) => c.Compare(y, x));
+		}
+	}
+
 	// クラスに型引数を指定することで、Create メソッドを呼び出すときに型引数 <T, Tkey> の指定を省略できます。
 	public static class ComparerHelper<T>
 	{
-		static IComparer<TKey> GetDefault<TKey>()
-		{
-			if (typeof(TKey) == typeof(string)) return (IComparer<TKey>)StringComparer.Ordinal;
-			return Comparer<TKey>.Default;
-		}
-
 		public static IComparer<T> Create(bool descending = false)
 		{
-			var c = GetDefault<T>();
-			if (descending) return Comparer<T>.Create((x, y) => c.Compare(y, x));
+			var c = ComparerHelper.GetDefault<T>();
+			if (descending) return c.ToDescending();
 			else return c;
 		}
 
 		public static IComparer<T> Create<TKey>(Func<T, TKey> keySelector, bool descending = false)
 		{
 			if (keySelector == null) throw new ArgumentNullException(nameof(keySelector));
-			var c = GetDefault<TKey>();
+			var c = ComparerHelper.GetDefault<TKey>();
 			if (descending) return Comparer<T>.Create((x, y) => c.Compare(keySelector(y), keySelector(x)));
 			else return Comparer<T>.Create((x, y) => c.Compare(keySelector(x), keySelector(y)));
 		}
@@ -31,8 +41,8 @@ namespace AlgorithmLab.Collections
 		{
 			if (keySelector1 == null) throw new ArgumentNullException(nameof(keySelector1));
 			if (keySelector2 == null) throw new ArgumentNullException(nameof(keySelector2));
-			var c1 = GetDefault<TKey1>();
-			var c2 = GetDefault<TKey2>();
+			var c1 = ComparerHelper.GetDefault<TKey1>();
+			var c2 = ComparerHelper.GetDefault<TKey2>();
 			return Comparer<T>.Create((x, y) =>
 			{
 				var d = c1.Compare(keySelector1(x), keySelector1(y));
