ae23eebffcf43913abd19fd7f0f80fb988457e75
Keiho Sakapon <git@saka-pon.net>
2022-06-21 23:10:33 +0900

    Add classes for Union-Find

---
+++ AlgorithmSample/AlgorithmLab/DataTrees/UF/UnionFind201.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace AlgorithmLab.DataTrees.UF
+{
+	// path compression
+	// O(α(n))
+	public class UnionFind201
+	{
+		int[] parents;
+
+		public UnionFind201(int n)
+		{
+			parents = new int[n];
+			Array.Fill(parents, -1);
+		}
+
+		public int Find(int x) => parents[x] == -1 ? x : parents[x] = Find(parents[x]);
+		public bool AreSame(int x, int y) => Find(x) == Find(y);
+
+		public bool Union(int x, int y)
+		{
+			if ((x = Find(x)) == (y = Find(y))) return false;
+
+			parents[y] = x;
+			return true;
+		}
+	}
+}
