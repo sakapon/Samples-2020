b5adf397da3f1fcd7d0d2213c117eea981626267
Keiho Sakapon <git@saka-pon.net>
2022-06-21 23:18:52 +0900

    Add classes for Union-Find

---
+++ AlgorithmSample/AlgorithmLab/DataTrees/UF/UnionFind301.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace AlgorithmLab.DataTrees.UF301
+{
+	// union by size, path compression
+	// O(log n)
+	public class UnionFind
+	{
+		int[] parents, sizes;
+
+		public UnionFind(int n)
+		{
+			parents = new int[n];
+			Array.Fill(parents, -1);
+			sizes = new int[n];
+			Array.Fill(sizes, 1);
+		}
+
+		public int Find(int x) => parents[x] == -1 ? x : parents[x] = Find(parents[x]);
+		public bool AreSame(int x, int y) => Find(x) == Find(y);
+		public int GetSize(int x) => sizes[Find(x)];
+
+		public bool Union(int x, int y)
+		{
+			if ((x = Find(x)) == (y = Find(y))) return false;
+
+			if (sizes[x] < sizes[y]) Merge(y, x);
+			else Merge(x, y);
+			return true;
+		}
+
+		void Merge(int x, int y)
+		{
+			parents[y] = x;
+			sizes[x] += sizes[y];
+		}
+	}
+}
---
+++ AlgorithmSample/AlgorithmLab/DataTrees/UF/UnionFind302.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace AlgorithmLab.DataTrees.UF302
+{
+	// union by rank, path compression
+	// O(log n)
+	public class UnionFind
+	{
+		int[] parents, ranks;
+
+		public UnionFind(int n)
+		{
+			parents = new int[n];
+			Array.Fill(parents, -1);
+			ranks = new int[n];
+		}
+
+		public int Find(int x) => parents[x] == -1 ? x : parents[x] = Find(parents[x]);
+		public bool AreSame(int x, int y) => Find(x) == Find(y);
+
+		public bool Union(int x, int y)
+		{
+			if ((x = Find(x)) == (y = Find(y))) return false;
+
+			if (ranks[x] < ranks[y]) Merge(y, x);
+			else Merge(x, y);
+			return true;
+		}
+
+		void Merge(int x, int y)
+		{
+			parents[y] = x;
+			if (ranks[x] == ranks[y]) ++ranks[x];
+		}
+	}
+}
