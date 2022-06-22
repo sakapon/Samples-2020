33176c40a32bcc031ec76c43fe070fac65a06dc6
Keiho Sakapon <git@saka-pon.net>
2022-06-22 10:06:08 +0900

    Add classes for Union-Find

---
+++ AlgorithmSample/AlgorithmLab/DataTrees/UF/UnionFind401.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+
+namespace AlgorithmLab.DataTrees.UF401
+{
+	// union by size, path compression
+	// O(α(n))
+	public class UnionFind
+	{
+		int[] parents, sizes;
+		public int GroupsCount { get; private set; }
+
+		public UnionFind(int n)
+		{
+			parents = new int[n];
+			Array.Fill(parents, -1);
+			sizes = new int[n];
+			Array.Fill(sizes, 1);
+			GroupsCount = n;
+		}
+
+		public int Find(int x) => parents[x] == -1 ? x : parents[x] = Find(parents[x]);
+		public bool AreSame(int x, int y) => Find(x) == Find(y);
+		public int GetSize(int x) => sizes[Find(x)];
+
+		public bool Union(int x, int y)
+		{
+			if ((x = Find(x)) == (y = Find(y))) return false;
+
+			if (sizes[x] < sizes[y]) Merge(y, x);
+			else Merge(x, y);
+			return true;
+		}
+
+		void Merge(int x, int y)
+		{
+			parents[y] = x;
+			sizes[x] += sizes[y];
+			--GroupsCount;
+		}
+
+		public ILookup<int, int> ToGroups() => Enumerable.Range(0, parents.Length).ToLookup(Find);
+	}
+}
