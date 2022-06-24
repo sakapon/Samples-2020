9e38eabec9676deccf0bf3d7711462b41e2f14d2
Keiho Sakapon <git@saka-pon.net>
2022-06-24 11:08:09 +0900

    Add classes for Union-Find

---
+++ AlgorithmSample/AlgorithmLab/DataTrees/UF/UnionFind501.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+
+namespace AlgorithmLab.DataTrees.UF501
+{
+	// Int32 vertexes
+	public class UnionFind<TValue>
+	{
+		int[] parents, sizes;
+		public int GroupsCount { get; private set; }
+		TValue[] values;
+		Func<TValue, TValue, TValue> mergeValues;
+
+		public UnionFind(int n, TValue[] values, Func<TValue, TValue, TValue> mergeValues)
+		{
+			parents = new int[n];
+			Array.Fill(parents, -1);
+			sizes = new int[n];
+			Array.Fill(sizes, 1);
+			GroupsCount = n;
+			this.values = values;
+			this.mergeValues = mergeValues;
+		}
+
+		public int Find(int x) => parents[x] == -1 ? x : parents[x] = Find(parents[x]);
+		public bool AreSame(int x, int y) => Find(x) == Find(y);
+		public int GetSize(int x) => sizes[Find(x)];
+		public TValue GetValue(int x) => values[Find(x)];
+
+		public bool Union(int x, int y)
+		{
+			if ((x = Find(x)) == (y = Find(y))) return false;
+
+			if (sizes[x] < sizes[y]) Merge(y, x);
+			else Merge(x, y);
+			return true;
+		}
+
+		void Merge(int x, int y)
+		{
+			parents[y] = x;
+			sizes[x] += sizes[y];
+			--GroupsCount;
+			values[x] = mergeValues(values[x], values[y]);
+		}
+
+		public ILookup<int, int> ToGroups() => Enumerable.Range(0, parents.Length).ToLookup(Find);
+	}
+}
---
+++ AlgorithmSample/AlgorithmLab/DataTrees/UF/UnionFind502.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AlgorithmLab.DataTrees.UF502
+{
+	// typed vertexes
+	// 頂点を動的に追加できる方式
+	[System.Diagnostics.DebuggerDisplay(@"Count = {Count}, GroupsCount = {GroupsCount}")]
+	public class UnionFind<TVertex, TValue>
+	{
+		[System.Diagnostics.DebuggerDisplay(@"\{{Item}\}")]
+		public class Node
+		{
+			public TVertex Item;
+			public TValue Value;
+			public Node Parent;
+			public int Size = 1;
+		}
+
+		Dictionary<TVertex, Node> nodes = new Dictionary<TVertex, Node>();
+		public int Count => nodes.Count;
+		public int GroupsCount { get; private set; }
+		TValue iv;
+		Func<TValue, TValue, TValue> mergeValues;
+
+		public UnionFind(TValue iv, Func<TValue, TValue, TValue> mergeValues, IEnumerable<KeyValuePair<TVertex, TValue>> collection = null)
+		{
+			if (collection != null)
+				foreach (var p in collection) nodes[p.Key] = new Node { Item = p.Key, Value = p.Value };
+			GroupsCount = nodes.Count;
+			this.iv = iv;
+			this.mergeValues = mergeValues;
+		}
+
+		public Node Add(TVertex x, TValue value)
+		{
+			if (nodes.ContainsKey(x)) return null;
+			++GroupsCount;
+			return nodes[x] = new Node { Item = x, Value = value };
+		}
+
+		public Node Find(TVertex x) => nodes.ContainsKey(x) ? Find(nodes[x]) : null;
+		Node Find(Node n) => n.Parent == null ? n : n.Parent = Find(n.Parent);
+
+		public bool AreSame(TVertex x, TVertex y)
+		{
+			if (nodes.Comparer.Equals(x, y)) return true;
+			var nx = Find(x);
+			var ny = Find(y);
+			return nx != null && nx == ny;
+		}
+		public int? GetSize(TVertex x) => Find(x)?.Size;
+		public TValue GetValue(TVertex x)
+		{
+			var n = Find(x);
+			return n != null ? n.Value : iv;
+		}
+
+		public bool Union(TVertex x, TVertex y)
+		{
+			var nx = Find(x) ?? Add(x, iv);
+			var ny = Find(y) ?? Add(y, iv);
+			if (nx == ny) return false;
+
+			if (nx.Size < ny.Size) Merge(ny, nx);
+			else Merge(nx, ny);
+			return true;
+		}
+
+		void Merge(Node nx, Node ny)
+		{
+			ny.Parent = nx;
+			nx.Size += ny.Size;
+			--GroupsCount;
+			nx.Value = mergeValues(nx.Value, ny.Value);
+		}
+
+		public ILookup<Node, Node> ToGroups() => nodes.Values.ToLookup(Find);
+	}
+}
