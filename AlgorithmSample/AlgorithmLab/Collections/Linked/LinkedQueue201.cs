9d88429cdf5d7997d33e5a0631f9d76d2c6b7083
Keiho Sakapon <git@saka-pon.net>
2022-07-06 20:58:30 +0900

    Add classes for collections

---
+++ AlgorithmSample/AlgorithmLab/Collections/Linked/LinkedQueue201.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AlgorithmLab.Collections.Linked.LinkedQueue201
+{
+	// singly linked list
+	[System.Diagnostics.DebuggerDisplay(@"Count = {Count}")]
+	public class LinkedQueue<T> : IEnumerable<T>
+	{
+		[System.Diagnostics.DebuggerDisplay(@"\{{Item}\}")]
+		class Node
+		{
+			public T Item;
+			public Node Next;
+		}
+
+		Node First;
+		Node Last;
+		public int Count { get; private set; }
+
+		public T Top => First != null ? First.Item : throw new InvalidOperationException("There are no items.");
+
+		public T Pop()
+		{
+			var item = Top;
+			if ((First = First.Next) == null) Last = null;
+			--Count;
+			return item;
+		}
+
+		public void Add(T item)
+		{
+			var node = new Node { Item = item };
+			Last = (Last == null) ? (First = node) : (Last.Next = node);
+			++Count;
+		}
+
+		System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator() => GetEnumerator();
+		public IEnumerator<T> GetEnumerator() { for (var n = First; n != null; n = n.Next) yield return n.Item; }
+	}
+}
---
+++ AlgorithmSample/AlgorithmLab/Collections/Linked/LinkedStack201.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AlgorithmLab.Collections.Linked.LinkedStack201
+{
+	// singly linked list
+	[System.Diagnostics.DebuggerDisplay(@"Count = {Count}")]
+	public class LinkedStack<T> : IEnumerable<T>
+	{
+		[System.Diagnostics.DebuggerDisplay(@"\{{Item}\}")]
+		class Node
+		{
+			public T Item;
+			public Node Next;
+		}
+
+		Node First;
+		public int Count { get; private set; }
+
+		public T Top => First != null ? First.Item : throw new InvalidOperationException("There are no items.");
+
+		public T Pop()
+		{
+			var item = Top;
+			First = First.Next;
+			--Count;
+			return item;
+		}
+
+		public void Add(T item)
+		{
+			First = new Node { Item = item, Next = First };
+			++Count;
+		}
+
+		System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator() => GetEnumerator();
+		public IEnumerator<T> GetEnumerator() { for (var n = First; n != null; n = n.Next) yield return n.Item; }
+	}
+}
