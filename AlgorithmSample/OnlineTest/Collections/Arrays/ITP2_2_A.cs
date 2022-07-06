b2a77bffaadfe3e30ea1476fae84fb88fb7a7418
Keiho Sakapon <git@saka-pon.net>
2022-07-06 10:35:42 +0900

    Add test

---
+++ AlgorithmSample/OnlineTest/Collections/Arrays/ITP2_2_A.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OnlineTest.Collections.Arrays
+{
+	class ITP2_2_A
+	{
+		static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+		static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
+		static void Main()
+		{
+			var (n, qc) = Read2();
+			var sb = new StringBuilder();
+
+			var qs = Array.ConvertAll(new bool[n], _ => new Stack<int>());
+
+			while (qc-- > 0)
+			{
+				var q = Read();
+				if (q[0] == 0)
+				{
+					qs[q[1]].Push(q[2]);
+				}
+				else if (q[0] == 1)
+				{
+					if (qs[q[1]].Count > 0) sb.Append(qs[q[1]].Peek()).AppendLine();
+				}
+				else
+				{
+					if (qs[q[1]].Count > 0) qs[q[1]].Pop();
+				}
+			}
+			Console.Write(sb);
+		}
+	}
+}
