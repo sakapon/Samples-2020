d9881ec5d4b542a43630b7fdff0a8e039c42af7a
Keiho Sakapon <git@saka-pon.net>
2022-07-16 20:04:18 +0900

    Add classes for PQ

---
+++ AlgorithmSample/AlgorithmLab/DataTrees/PQ/HeapQueue101.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Test: https://onlinejudge.u-aizu.ac.jp/courses/lesson/1/ALDS1/9/ALDS1_9_C
+// 容量を固定します。
+namespace AlgorithmLab.DataTrees.PQ.HeapQueue101
+{
+	public class HeapQueue<T>
+	{
+		T[] a;
+		int n = 1;
+		readonly IComparer<T> c;
+
+		public HeapQueue(int capacity, IComparer<T> comparer = null)
+		{
+			a = new T[capacity];
+			c = comparer ?? Comparer<T>.Default;
+		}
+
+		public int Count => n - 1;
+		public T First => a[1];
+
+		public void Clear() => n = 1;
+
+		public void Add(T item)
+		{
+			a[n++] = item;
+
+			for (int i = n - 1; i > 1 && c.Compare(a[i], a[i >> 1]) < 0; i >>= 1)
+			{
+				(a[i], a[i >> 1]) = (a[i >> 1], a[i]);
+			}
+		}
+
+		public T Pop()
+		{
+			var item = a[1];
+			a[1] = a[--n];
+
+			for (int i = 2; i < n; i <<= 1)
+			{
+				if (i + 1 < n && c.Compare(a[i], a[i + 1]) > 0) ++i;
+				if (c.Compare(a[i], a[i >> 1]) >= 0) break;
+				(a[i], a[i >> 1]) = (a[i >> 1], a[i]);
+			}
+			return item;
+		}
+	}
+}
---
+++ AlgorithmSample/OnlineTest/DataTrees/PQ/ALDS1_9_C.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AlgorithmLab.DataTrees.PQ.HeapQueue101;
+
+namespace OnlineTest.DataTrees.PQ
+{
+	// AOJ 向けに変更するコード
+	//var t = a[i]; a[i] = a[i >> 1]; a[i >> 1] = t;
+	// Test: https://onlinejudge.u-aizu.ac.jp/courses/lesson/1/ALDS1/9/ALDS1_9_C
+	class ALDS1_9_C
+	{
+		static void Main()
+		{
+			var sb = new StringBuilder();
+
+			var asc = Comparer<int>.Default;
+			var desc = Comparer<int>.Create((x, y) => asc.Compare(y, x));
+			var q = new HeapQueue<int>(2000000, desc);
+
+			string s;
+			while ((s = Console.ReadLine())[2] != 'd')
+			{
+				if (s[0] == 'i')
+				{
+					q.Add(int.Parse(s.Substring(7)));
+				}
+				else
+				{
+					sb.Append(q.Pop()).AppendLine();
+				}
+			}
+			Console.Write(sb);
+		}
+	}
+}
