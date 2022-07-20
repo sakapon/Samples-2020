76dcd54ab2c67203506a73b5002e935ba141c171
Keiho Sakapon <git@saka-pon.net>
2022-07-20 10:12:10 +0900

    Add test

---
+++ AlgorithmSample/OnlineTest/DataTrees/UF/ABC177_D.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using AlgorithmLab.DataTrees.UF401;
+
+namespace OnlineTest.DataTrees.UF
+{
+	// Test: https://atcoder.jp/contests/abc177/tasks/abc177_d
+	class ABC177_D
+	{
+		static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+		static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
+		static void Main() => Console.WriteLine(Solve());
+		static object Solve()
+		{
+			var (n, m) = Read2();
+			var es = Array.ConvertAll(new bool[m], _ => Read2());
+
+			var uf = new UnionFind(n + 1);
+			foreach (var (a, b) in es)
+			{
+				uf.Union(a, b);
+			}
+			return Enumerable.Range(1, n).Max(uf.GetSize);
+		}
+	}
+}
---
+++ AlgorithmSample/OnlineTest/DataTrees/UF/ARC106_B.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using AlgorithmLab.DataTrees.UF401;
+
+namespace OnlineTest.DataTrees.UF
+{
+	// Test: https://atcoder.jp/contests/arc106/tasks/arc106_b
+	class ARC106_B
+	{
+		static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+		static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
+		static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
+		static void Main() => Console.WriteLine(Solve() ? "Yes" : "No");
+		static bool Solve()
+		{
+			var (n, m) = Read2();
+			var a = ReadL();
+			var b = ReadL();
+			var es = Array.ConvertAll(new bool[m], _ => Read2());
+
+			var uf = new UnionFind(n);
+			foreach (var (c, d) in es)
+			{
+				uf.Union(c - 1, d - 1);
+			}
+
+			return uf.ToGroups().All(g => g.Sum(v => a[v]) == g.Sum(v => b[v]));
+		}
+	}
+}
