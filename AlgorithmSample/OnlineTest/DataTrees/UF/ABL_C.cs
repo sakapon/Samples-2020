8769c57211341cd7a33b9afd6cfe4078c4a72cf5
Keiho Sakapon <git@saka-pon.net>
2022-12-22 19:41:57 +0900

    Add test

---
+++ AlgorithmSample/OnlineTest/DataTrees/UF/ABL_C.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using AlgorithmLab.DataTrees.UF401;
+
+namespace OnlineTest.DataTrees.UF
+{
+	// Test: https://atcoder.jp/contests/abl/tasks/abl_c
+	class ABL_C
+	{
+		static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+		static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
+		static void Main() => Console.WriteLine(Solve());
+		static object Solve()
+		{
+			var (n, m) = Read2();
+			var es = Array.ConvertAll(new bool[m], _ => Read2());
+
+			var uf = new UnionFind(n + 1);
+			foreach (var (a, b) in es)
+				uf.Union(a, b);
+			return uf.GroupsCount - 2;
+		}
+	}
+}
