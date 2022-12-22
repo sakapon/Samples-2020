5b518a771fc0a3578b3bc426b12090b1be46883e
Keiho Sakapon <git@saka-pon.net>
2022-12-22 15:04:33 +0900

    Add class with data augmentation

---
+++ AlgorithmSample/AlgorithmLab/DataTrees/UF/UnionFind506.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Linq;
+
+namespace AlgorithmLab.DataTrees.UF506
+{
+	// Int32 vertexes, data augmentation
+	// parents[root] = -size
+	public class UnionFind<TValue>
+	{
+		readonly int[] parents;
+		int groupsCount;
+		readonly Func<TValue, TValue, TValue> mergeValues;
+		readonly TValue[] values;
+
+		public UnionFind(int n, Func<TValue, TValue, TValue> mergeValues, TValue[] values = null)
+		{
+			parents = new int[n];
+			Array.Fill(parents, -1);
+			groupsCount = n;
+			this.mergeValues = mergeValues;
+			this.values = new TValue[n];
+			if (values != null) Array.Copy(values, this.values, n);
+		}
+
+		public int ItemsCount => parents.Length;
+		public int GroupsCount => groupsCount;
+		public int Find(int x) => parents[x] < 0 ? x : parents[x] = Find(parents[x]);
+		public bool AreSame(int x, int y) => Find(x) == Find(y);
+		public int GetSize(int x) => -parents[Find(x)];
+
+		public TValue this[int x]
+		{
+			get => values[Find(x)];
+			set => values[Find(x)] = value;
+		}
+
+		public bool Union(int x, int y)
+		{
+			if ((x = Find(x)) == (y = Find(y))) return false;
+
+			// 左右の順序を保って値をマージします。
+			var v = mergeValues(values[x], values[y]);
+
+			if (parents[x] > parents[y]) (x, y) = (y, x);
+			parents[x] += parents[y];
+			parents[y] = x;
+			--groupsCount;
+			values[x] = v;
+			return true;
+		}
+
+		public ILookup<int, int> ToGroups() => Enumerable.Range(0, parents.Length).ToLookup(Find);
+	}
+}
