d1ffa2fb09d404aed1e9167ccba0351639260df7
Keiho Sakapon <git@saka-pon.net>
2022-12-23 10:54:12 +0900

    Add test

---
+++ AlgorithmSample/OnlineTest/Graphs/SPPs/YJLC_SP.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using CoderLib8.Graphs.SPPs.Int.WeightedGraph211;
+
+namespace OnlineTest.Graphs.SPPs
+{
+	// Test: https://judge.yosupo.jp/problem/shortest_path
+	class YJLC_SP
+	{
+		static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+		static (int, int, int) Read3() { var a = Read(); return (a[0], a[1], a[2]); }
+		static (int, int, int, int) Read4() { var a = Read(); return (a[0], a[1], a[2], a[3]); }
+		static void Main() => Console.WriteLine(Solve());
+		static object Solve()
+		{
+			var (n, m, s, t) = Read4();
+			var es = Array.ConvertAll(new bool[m], _ => Read3());
+
+			var graph = new WeightedGraph(n, es, false);
+			graph.Dijkstra(s, t);
+			if (!graph[t].IsConnected) return -1;
+
+			var path = graph.GetPathEdges(t);
+			return $"{graph[t].Cost} {path.Length}\n" + string.Join("\n", path.Select(e => $"{e.Item1} {e.Item2}"));
+		}
+	}
+}
