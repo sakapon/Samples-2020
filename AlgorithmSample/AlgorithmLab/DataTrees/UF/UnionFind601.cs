83b0095acb61510d19bf7064a857817a70642884
Keiho Sakapon <git@saka-pon.net>
2023-03-27 17:16:32 +0900

    Add UnionFind 601

---
+++ AlgorithmSample/AlgorithmLab/DataTrees/UF/UnionFind601.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Linq;
+
+namespace AlgorithmLab.DataTrees.UF601
+{
+	// Int32 vertexes
+	[System.Diagnostics.DebuggerDisplay(@"ItemsCount = {ItemsCount}, SetsCount = {SetsCount}")]
+	public class UnionFind<TValue>
+	{
+		[System.Diagnostics.DebuggerDisplay(@"\{{Key}, {Value}\}")]
+		public class Node
+		{
+			public int Key;
+			public TValue Value;
+			public Node Parent;
+			public int Size = 1;
+		}
+
+		readonly Node[] nodes;
+		readonly Func<TValue, TValue, TValue> addValues;
+		readonly Func<int, int, TValue> diff;
+
+		public UnionFind(int n, Func<TValue, TValue, TValue> addValues, Func<int, int, TValue> diff)
+		{
+			nodes = new Node[n];
+			for (int i = 0; i < n; ++i) nodes[i] = new Node { Key = i };
+			SetsCount = n;
+			this.addValues = addValues;
+			this.diff = diff;
+		}
+
+		public int ItemsCount => nodes.Length;
+		public int SetsCount { get; private set; }
+
+		public TValue this[int x]
+		{
+			get
+			{
+				Find(x);
+				return nodes[x].Value;
+			}
+		}
+
+		public Node Find(int x) => Find(nodes[x]);
+		Node Find(Node n)
+		{
+			if (n.Parent == null)
+			{
+				return n;
+			}
+			else
+			{
+				var root = Find(n.Parent);
+				n.Value = addValues(n.Parent.Value, n.Value);
+				return n.Parent = root;
+			}
+		}
+
+		public bool AreSame(int x, int y) => Find(x) == Find(y);
+		public bool Union(int x, int y)
+		{
+			var nx = Find(x);
+			var ny = Find(y);
+			if (nx == ny) return false;
+
+			if (nx.Size < ny.Size) (nx, ny) = (ny, nx);
+			ny.Parent = nx;
+			ny.Value = diff(nx.Key, ny.Key);
+			nx.Size += ny.Size;
+			--SetsCount;
+			return true;
+		}
+
+		public ILookup<Node, Node> ToGroups() => nodes.ToLookup(Find);
+	}
+}
---
+++ AlgorithmSample/UnitTest/DataTrees/UF/UnionFind601Test.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using AlgorithmLab.DataTrees.UF601;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTest.DataTrees.UF
+{
+	[TestClass]
+	public class UnionFind601Test
+	{
+		[TestMethod]
+		public void Distance()
+		{
+			var n = 13;
+			var uf = new UnionFind<int>(n, (x, y) => x + y, (x, y) => y - x);
+
+			uf.Union(6, 8);
+			uf.Union(9, 11);
+			uf.Union(8, 11);
+			uf.Union(5, 1);
+			uf.Union(4, 12);
+			uf.Union(5, 4);
+			uf.Union(9, 12);
+
+			for (int i = 0; i < n; i++)
+			{
+				var root = uf.Find(i);
+				Assert.AreEqual(i - root.Key, uf[i]);
+			}
+		}
+
+		[TestMethod]
+		public void Distance_Random()
+		{
+			var random = new Random();
+			var n = 100000;
+			var uf = new UnionFind<int>(n, (x, y) => x + y, (x, y) => y - x);
+
+			for (int i = 0; i < 100000; i++)
+			{
+				uf.Union(random.Next(n), random.Next(n));
+			}
+
+			for (int i = 0; i < n; i++)
+			{
+				var root = uf.Find(i);
+				Assert.AreEqual(i - root.Key, uf[i]);
+			}
+		}
+	}
+}
