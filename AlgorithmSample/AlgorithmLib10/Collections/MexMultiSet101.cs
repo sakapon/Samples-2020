44ed4565f7ff27aab33409f52f1c0a3008c1f693
Keiho Sakapon <git@saka-pon.net>
2023-12-01 22:32:56 +0900

    Add classes for MexMultiSet

---
+++ AlgorithmSample/AlgorithmLib10/Collections/MexMultiSet101.cs
@@ -0,0 +1,34 @@
+﻿namespace AlgorithmLib10.Collections.MexMultiSet101
+{
+	[System.Diagnostics.DebuggerDisplay(@"Mex = {Mex}")]
+	public class MexMultiSet
+	{
+		readonly int max;
+		readonly int[] counts;
+		readonly SortedSet<int> set;
+
+		public MexMultiSet(int max = 1 << 18)
+		{
+			this.max = max;
+			counts = new int[max];
+			set = new SortedSet<int>(Enumerable.Range(0, max));
+		}
+
+		public int Mex => set.Count == 0 ? max : set.Min;
+
+		public bool Add(int value)
+		{
+			if (value < 0 || max <= value) return false;
+			if (counts[value]++ == 0) set.Remove(value);
+			return true;
+		}
+
+		public bool Remove(int value)
+		{
+			if (value < 0 || max <= value) return false;
+			if (counts[value] == 0) return false;
+			if (--counts[value] == 0) set.Add(value);
+			return true;
+		}
+	}
+}
--- AlgorithmSample/AlgorithmLib10/Collections/MexMultiSet102.cs
+++ AlgorithmSample/AlgorithmLib10/Collections/MexMultiSet102.cs
@@ -1,17 +1,19 @@
-﻿namespace AlgorithmLib10.Collections.MexSet102
+﻿namespace AlgorithmLib10.Collections.MexMultiSet102
 {
 	[System.Diagnostics.DebuggerDisplay(@"Count = {Count}, Mex = {Mex}")]
-	public class MexSet
+	public class MexMultiSet
 	{
 		readonly bool errorForArgs;
 		readonly int max;
+		readonly int[] counts;
 		readonly int[] range;
 		SortedSet<int> set;
 
-		public MexSet(int max = 1 << 18, IEnumerable<int>? collection = null, bool errorForArgs = false)
+		public MexMultiSet(int max = 1 << 18, IEnumerable<int>? collection = null, bool errorForArgs = false)
 		{
 			this.errorForArgs = errorForArgs;
 			this.max = max;
+			counts = new int[max];
 			range = new int[max];
 			for (int i = 0; i < max; ++i) range[i] = i;
 			Clear();
@@ -19,7 +21,7 @@
 		}
 
 		public int Mex => set.Count == 0 ? max : set.Min;
-		public int Count => max - set.Count;
+		public int Count { get; private set; }
 
 		public void Clear()
 		{
@@ -36,19 +38,24 @@
 		public bool Add(int value)
 		{
 			if (!Validate(value)) return false;
-			return set.Remove(value);
+			if (counts[value]++ == 0) set.Remove(value);
+			++Count;
+			return true;
 		}
 
 		public bool Remove(int value)
 		{
 			if (!Validate(value)) return false;
-			return set.Add(value);
+			if (counts[value] == 0) return false;
+			if (--counts[value] == 0) set.Add(value);
+			--Count;
+			return true;
 		}
 
-		public bool Contains(int value)
+		public int GetCount(int value)
 		{
-			if (!Validate(value)) return false;
-			return !set.Contains(value);
+			if (!Validate(value)) return -1;
+			return counts[value];
 		}
 	}
 }
