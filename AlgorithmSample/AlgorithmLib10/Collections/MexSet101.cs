5211e4f580b23f21fd594541baa66f80ca4f233c
Keiho Sakapon <git@saka-pon.net>
2023-12-01 18:58:45 +0900

    Add classes for MexSet

---
+++ AlgorithmSample/AlgorithmLib10/Collections/MexSet101.cs
@@ -0,0 +1,29 @@
+﻿namespace AlgorithmLib10.Collections.MexSet101
+{
+	[System.Diagnostics.DebuggerDisplay(@"Mex = {Mex}")]
+	public class MexSet
+	{
+		readonly int max;
+		readonly SortedSet<int> set;
+
+		public MexSet(int max = 1 << 18)
+		{
+			this.max = max;
+			set = new SortedSet<int>(Enumerable.Range(0, max));
+		}
+
+		public int Mex => set.Count == 0 ? max : set.Min;
+
+		public bool Add(int value)
+		{
+			if (value < 0 || max <= value) return false;
+			return set.Remove(value);
+		}
+
+		public bool Remove(int value)
+		{
+			if (value < 0 || max <= value) return false;
+			return set.Add(value);
+		}
+	}
+}
---
+++ AlgorithmSample/AlgorithmLib10/Collections/MexSet102.cs
@@ -0,0 +1,54 @@
+﻿namespace AlgorithmLib10.Collections.MexSet102
+{
+	[System.Diagnostics.DebuggerDisplay(@"Mex = {Mex}")]
+	public class MexSet
+	{
+		readonly bool errorForArgs;
+		readonly int max;
+		readonly int[] range;
+		SortedSet<int> set;
+
+		public MexSet(int max = 1 << 18, IEnumerable<int>? collection = null, bool errorForArgs = false)
+		{
+			this.errorForArgs = errorForArgs;
+			this.max = max;
+			range = new int[max];
+			for (int i = 0; i < max; ++i) range[i] = i;
+			Clear();
+			if (collection != null) foreach (var v in collection) Add(v);
+		}
+
+		public int Mex => set.Count == 0 ? max : set.Min;
+		public int Count => max - set.Count;
+
+		public void Clear()
+		{
+			set = new SortedSet<int>(range);
+		}
+
+		bool Validate(int value)
+		{
+			if (0 <= value && value < max) return true;
+			if (errorForArgs) throw new ArgumentOutOfRangeException(nameof(value));
+			else return false;
+		}
+
+		public bool Add(int value)
+		{
+			if (!Validate(value)) return false;
+			return set.Remove(value);
+		}
+
+		public bool Remove(int value)
+		{
+			if (!Validate(value)) return false;
+			return set.Add(value);
+		}
+
+		public bool Contains(int value)
+		{
+			if (!Validate(value)) return false;
+			return !set.Contains(value);
+		}
+	}
+}
