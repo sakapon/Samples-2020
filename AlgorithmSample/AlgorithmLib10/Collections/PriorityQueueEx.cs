023ad0db6efb7c9a672f014203aeae6e8a9e73e7
Keiho Sakapon <git@saka-pon.net>
2023-12-02 19:10:07 +0900

    Add PriorityQueueEx class

---
+++ AlgorithmSample/AlgorithmLib10/Collections/PriorityQueueEx.cs
@@ -0,0 +1,40 @@
+﻿namespace AlgorithmLib10.Collections
+{
+	public class PriorityQueueEx<T> : PriorityQueue<T, T> where T : notnull
+	{
+		readonly Dictionary<T, int> removed = new();
+
+		public PriorityQueueEx() { }
+		public PriorityQueueEx(IEnumerable<T> items) : base(items.Select(v => (v, v))) { }
+
+		public void Enqueue(T item)
+		{
+			base.Enqueue(item, item);
+		}
+
+		public new T Dequeue()
+		{
+			var r = base.Dequeue();
+			EnsureFirst();
+			return r;
+		}
+
+		public void Remove(T item)
+		{
+			removed[item] = removed.GetValueOrDefault(item) + 1;
+			EnsureFirst();
+		}
+
+		void EnsureFirst()
+		{
+			while (Count > 0)
+			{
+				var item = Peek();
+				if (!removed.TryGetValue(item, out var c)) break;
+				Dequeue();
+				if (--c == 0) removed.Remove(item);
+				else removed[item] = c;
+			}
+		}
+	}
+}
