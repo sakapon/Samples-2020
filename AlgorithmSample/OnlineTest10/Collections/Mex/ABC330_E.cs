4c125a5d2d581e62778048aada8dc216708a5da0
Keiho Sakapon <git@saka-pon.net>
2023-12-05 16:20:45 +0900

    Add test

---
+++ AlgorithmSample/OnlineTest10/Collections/Mex/ABC330_E.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using AlgorithmLib10.Collections.MexMultiSet102;
+
+namespace OnlineTest10.Collections.Mex
+{
+	// Test: https://atcoder.jp/contests/abc330/tasks/abc330_e
+	class ABC330_E
+	{
+		static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+		static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
+		static void Main() => Console.WriteLine(Solve());
+		static object Solve()
+		{
+			var (n, qc) = Read2();
+			var a = Read();
+			var qs = Array.ConvertAll(new bool[qc], _ => Read());
+
+			var set = new MexMultiSet(200000, a);
+			return string.Join("\n", qs.Select(q => Query(q[0] - 1, q[1])));
+
+			int Query(int i, int x)
+			{
+				set.Remove(a[i]);
+				a[i] = x;
+				set.Add(a[i]);
+				return set.Mex;
+			}
+		}
+	}
+}
