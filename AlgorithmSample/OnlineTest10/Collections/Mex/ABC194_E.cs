cb5500eac85d4d37e6e5ccb5176f44d06fea64d3
Keiho Sakapon <git@saka-pon.net>
2023-12-05 17:12:07 +0900

    Add test

--- AlgorithmSample/OnlineTest10/Collections/Mex/ABC194_E.cs
+++ AlgorithmSample/OnlineTest10/Collections/Mex/ABC194_E.cs
@@ -2,28 +2,27 @@
 
 namespace OnlineTest10.Collections.Mex
 {
-	// Test: https://atcoder.jp/contests/abc330/tasks/abc330_e
-	class ABC330_E
+	// Test: https://atcoder.jp/contests/abc194/tasks/abc194_e
+	class ABC194_E
 	{
 		static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
 		static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
 		static void Main() => Console.WriteLine(Solve());
 		static object Solve()
 		{
-			var (n, qc) = Read2();
+			var (n, m) = Read2();
 			var a = Read();
-			var qs = Array.ConvertAll(new bool[qc], _ => Read());
 
-			var set = new MexMultiSet(200000, a);
-			return string.Join("\n", qs.Select(q => Query(q[0] - 1, q[1])));
+			var set = new MexMultiSet(m, a[0..m]);
+			var r = set.Mex;
 
-			int Query(int i, int x)
+			for (int i = 0; i + m < n; i++)
 			{
 				set.Remove(a[i]);
-				a[i] = x;
-				set.Add(a[i]);
-				return set.Mex;
+				set.Add(a[i + m]);
+				r = Math.Min(r, set.Mex);
 			}
+			return r;
 		}
 	}
 }
