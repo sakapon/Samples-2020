0a1d091b332d6e6347d799e9e26146fc812fa3c4
Keiho Sakapon <git@saka-pon.net>
2023-12-30 12:01:52 +0900

    Add ListHeapQueueMap class

---
+++ AlgorithmSample/AlgorithmLib10/DataTrees/PQ/ListHeapQueueMap202.cs
@@ -0,0 +1,57 @@
+﻿// 0-based, bit operations
+namespace AlgorithmLib10.DataTrees.PQ.ListHeapQueueMap202
+{
+	[System.Diagnostics.DebuggerDisplay(@"Count = {Count}")]
+	public class ListHeapQueueMap<TKey, TValue>
+	{
+		readonly IComparer<TKey> c;
+		readonly int s;
+		readonly List<TKey> l = new List<TKey>();
+		readonly List<TValue> v = new List<TValue>();
+
+		public ListHeapQueueMap(IComparer<TKey> comparer = null, bool descending = false, IEnumerable<(TKey key, TValue value)> items = null)
+		{
+			c = comparer ?? Comparer<TKey>.Default;
+			s = descending ? -1 : 1;
+			if (items != null) foreach (var (k, v) in items) Push(k, v);
+		}
+
+		public IComparer<TKey> Comparer => c;
+		public bool Descending => s == -1;
+		public IEnumerable<(TKey key, TValue value)> GetRawItems => l.Zip(v);
+		public int Count => l.Count;
+		public (TKey key, TValue value) First => l.Count != 0 ? (l[0], v[0]) : throw new InvalidOperationException("No items.");
+
+		public void Clear() { l.Clear(); v.Clear(); }
+
+		bool TrySwap(int i)
+		{
+			var p = (i - 1) >> 1;
+			if (s * c.Compare(l[p], l[i]) <= 0) return false;
+			(l[i], l[p]) = (l[p], l[i]);
+			(v[i], v[p]) = (v[p], v[i]);
+			return true;
+		}
+		void UpHeap() { for (var i = l.Count - 1; i != 0 && TrySwap(i); i = (i - 1) >> 1) ; }
+		void DownHeap() { for (var i = 1; i < l.Count && TrySwap(i + 1 < l.Count && s * c.Compare(l[i + 1], l[i]) < 0 ? ++i : i); i = (i << 1) | 1) ; }
+
+		public void Push(TKey key, TValue value)
+		{
+			l.Add(key);
+			v.Add(value);
+			UpHeap();
+		}
+
+		public (TKey key, TValue value) Pop()
+		{
+			if (l.Count == 0) throw new InvalidOperationException("No items.");
+			var item = (l[0], v[0]);
+			l[0] = l[l.Count - 1];
+			v[0] = v[v.Count - 1];
+			l.RemoveAt(l.Count - 1);
+			v.RemoveAt(v.Count - 1);
+			DownHeap();
+			return item;
+		}
+	}
+}
---
+++ AlgorithmSample/OnlineTest10/DataTrees/PQ/ABC323_D.cs
@@ -0,0 +1,33 @@
+using AlgorithmLib10.DataTrees.PQ.ListHeapQueueMap202;
+
+namespace OnlineTest10.DataTrees.PQ
+{
+	// Test: https://atcoder.jp/contests/abc323/tasks/abc323_d
+	class ABC323_D
+	{
+		static long[] ReadL() => Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
+		static (long, long) Read2L() { var a = ReadL(); return (a[0], a[1]); }
+		static void Main() => Console.WriteLine(Solve());
+		static object Solve()
+		{
+			var n = int.Parse(Console.ReadLine());
+			var ps = Array.ConvertAll(new bool[n], _ => Read2L());
+
+			var r = 0;
+			var map = new ListHeapQueueMap<long, long>(items: ps);
+
+			while (map.Count > 0)
+			{
+				var (s, c) = map.Pop();
+				while (map.Count > 0 && map.First.key == s)
+					c += map.Pop().value;
+
+				if ((c & 1) != 0) r++;
+				s <<= 1;
+				c >>= 1;
+				if (c != 0) map.Push(s, c);
+			}
+			return r;
+		}
+	}
+}
