42de2b206cafb8966312ad80bc73d59c43c887c7
Keiho Sakapon <git@saka-pon.net>
2023-12-30 10:53:38 +0900

    Add Descending property

--- AlgorithmSample/AlgorithmLib10/DataTrees/PQ/ListHeapQueue202.cs
+++ AlgorithmSample/AlgorithmLib10/DataTrees/PQ/ListHeapQueue202.cs
@@ -5,15 +5,18 @@ namespace AlgorithmLib10.DataTrees.PQ.ListHeapQueue202
 	public class ListHeapQueue<T>
 	{
 		readonly IComparer<T> c;
+		readonly int s;
 		readonly List<T> l = new List<T>();
 
-		public ListHeapQueue(IComparer<T> comparer = null, IEnumerable<T> items = null)
+		public ListHeapQueue(IComparer<T> comparer = null, bool descending = false, IEnumerable<T> items = null)
 		{
 			c = comparer ?? Comparer<T>.Default;
+			s = descending ? -1 : 1;
 			if (items != null) foreach (var v in items) Push(v);
 		}
 
 		public IComparer<T> Comparer => c;
+		public bool Descending => s == -1;
 		public List<T> Raw => l;
 		public int Count => l.Count;
 		public T First => l.Count != 0 ? l[0] : throw new InvalidOperationException("No items.");
@@ -23,12 +26,12 @@ namespace AlgorithmLib10.DataTrees.PQ.ListHeapQueue202
 		bool TrySwap(int i)
 		{
 			var p = (i - 1) >> 1;
-			if (c.Compare(l[p], l[i]) <= 0) return false;
+			if (s * c.Compare(l[p], l[i]) <= 0) return false;
 			(l[i], l[p]) = (l[p], l[i]);
 			return true;
 		}
 		void UpHeap() { for (var i = l.Count - 1; i != 0 && TrySwap(i); i = (i - 1) >> 1) ; }
-		void DownHeap() { for (var i = 1; i < l.Count && TrySwap(i + 1 < l.Count && c.Compare(l[i + 1], l[i]) < 0 ? ++i : i); i = (i << 1) | 1) ; }
+		void DownHeap() { for (var i = 1; i < l.Count && TrySwap(i + 1 < l.Count && s * c.Compare(l[i + 1], l[i]) < 0 ? ++i : i); i = (i << 1) | 1) ; }
 
 		public void Push(T item)
 		{
