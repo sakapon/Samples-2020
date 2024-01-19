ae1e48197a0d14c4b0b03fb19b84684d4d34f572
Keiho Sakapon <git@saka-pon.net>
2024-01-19 11:25:07 +0900

    Add test

---
+++ AlgorithmSample/OnlineTest/Collections/Hashing/ITP2_7_A.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using AlgorithmLab.Collections.ListHash101;
+
+namespace OnlineTest.Collections.Hashing
+{
+	// Test: https://onlinejudge.u-aizu.ac.jp/courses/lesson/8/ITP2/7/ITP2_7_A
+	class ITP2_7_A
+	{
+		static void Main()
+		{
+			var qc = int.Parse(Console.ReadLine());
+
+			var set = new ListHashSet<int>(1 << 20);
+
+			Console.SetOut(new System.IO.StreamWriter(Console.OpenStandardOutput()) { AutoFlush = false });
+			for (int i = 0; i < qc; i++)
+			{
+				var q = Console.ReadLine().Split();
+				var x = int.Parse(q[1]);
+
+				if (q[0][0] == '0')
+				{
+					set.Add(x);
+					Console.WriteLine(set.Count);
+				}
+				else
+				{
+					Console.WriteLine(set.Contains(x) ? 1 : 0);
+				}
+			}
+			Console.Out.Flush();
+		}
+	}
+}
---
+++ AlgorithmSample/OnlineTest/Collections/Hashing/ITP2_7_B.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using AlgorithmLab.Collections.ListHash101;
+
+namespace OnlineTest.Collections.Hashing
+{
+	// Test: https://onlinejudge.u-aizu.ac.jp/courses/lesson/8/ITP2/7/ITP2_7_B
+	class ITP2_7_B
+	{
+		static void Main()
+		{
+			var qc = int.Parse(Console.ReadLine());
+
+			var set = new ListHashSet<int>(1 << 20);
+
+			Console.SetOut(new System.IO.StreamWriter(Console.OpenStandardOutput()) { AutoFlush = false });
+			for (int i = 0; i < qc; i++)
+			{
+				var q = Console.ReadLine().Split();
+				var x = int.Parse(q[1]);
+
+				if (q[0][0] == '0')
+				{
+					set.Add(x);
+					Console.WriteLine(set.Count);
+				}
+				else if (q[0][0] == '1')
+				{
+					Console.WriteLine(set.Contains(x) ? 1 : 0);
+				}
+				else
+				{
+					set.Remove(x);
+				}
+			}
+			Console.Out.Flush();
+		}
+	}
+}
