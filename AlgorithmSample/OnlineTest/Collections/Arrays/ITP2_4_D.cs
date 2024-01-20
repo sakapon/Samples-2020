9d53ea19f46c2dd3d9c2176a2dd42785e74462ef
Keiho Sakapon <git@saka-pon.net>
2024-01-20 16:03:43 +0900

    Add test

---
+++ AlgorithmSample/OnlineTest/Collections/Arrays/ITP2_4_D.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using AlgorithmLab.Collections.Arrays201;
+
+namespace OnlineTest.Collections.Arrays
+{
+	// Test: https://onlinejudge.u-aizu.ac.jp/courses/lesson/8/ITP2/4/ITP2_4_D
+	class ITP2_4_D
+	{
+		static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+		static void Main() => Console.WriteLine(Solve());
+		static object Solve()
+		{
+			Console.ReadLine();
+			var a = Read();
+
+			var set = new ArrayHashSet<int>(1 << 19);
+			foreach (var x in a) set.Add(x);
+			a = set.ToArray();
+			Array.Sort(a);
+			return string.Join(" ", a);
+		}
+	}
+}
