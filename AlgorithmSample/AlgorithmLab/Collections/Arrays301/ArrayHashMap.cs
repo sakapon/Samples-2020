d4cd135908da70725f1556b659eae9df7a606d32
Keiho Sakapon <git@saka-pon.net>
2024-01-24 17:51:11 +0900

    Copy file

---
+++ AlgorithmSample/AlgorithmLab/Collections/Arrays301/ArrayHashMap.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AlgorithmLab.Collections.Arrays301
+{
+	[System.Diagnostics.DebuggerDisplay(@"Count = {Count}")]
+	public class ArrayHashMap<TKey, TValue> : IEnumerable<(TKey key, TValue value)>
+	{
+		int[] firsts, nexts;
+		bool[] u;
+		TKey[] keys;
+		TValue[] values;
+		int n, f;
+		readonly TValue iv;
+		readonly IEqualityComparer<TKey> ec = typeof(TKey) == typeof(string) ? (IEqualityComparer<TKey>)StringComparer.Ordinal : EqualityComparer<TKey>.Default;
+
+		public ArrayHashMap(TValue iv = default(TValue), int capacity = 8)
+		{
+			this.iv = iv;
+			var c = 1;
+			while (c < capacity) c <<= 1;
+			Initialize(c);
+		}
+
+		public int Count => n;
+		public void Clear() => Initialize(8);
+		void Initialize(int c)
+		{
+			firsts = new int[c];
+			nexts = new int[c];
+			u = new bool[c];
+			keys = new TKey[c];
+			values = new TValue[c];
+			Array.Fill(firsts, -1);
+			Array.Fill(nexts, -1);
+			n = 0;
+			f = c - 1;
+		}
+
+		int GetIndex(TKey key)
+		{
+			var h = key.GetHashCode() & f;
+			for (var i = firsts[h]; i != -1; i = nexts[i])
+				if (ec.Equals(keys[i], key)) return i;
+			return -1;
+		}
+
+		public TValue this[TKey key]
+		{
+			get
+			{
+				var i = GetIndex(key);
+				return i != -1 ? values[i] : iv;
+			}
+			set
+			{
+				if (n == keys.Length >> 1) Expand();
+				AddOrUpdate(key, value, true);
+			}
+		}
+
+		public bool ContainsKey(TKey key) => GetIndex(key) != -1;
+
+		public bool TryGetValue(TKey key, out TValue value)
+		{
+			var i = GetIndex(key);
+			if (i != -1) { value = values[i]; return true; }
+			else { value = iv; return false; }
+		}
+
+		public bool Add(TKey key, TValue value)
+		{
+			if (n == keys.Length >> 1) Expand();
+			return AddOrUpdate(key, value, false);
+		}
+
+		bool AddOrUpdate(TKey key, TValue value, bool update)
+		{
+			var h = key.GetHashCode() & f;
+			var last = -1;
+			var i = firsts[h];
+			for (; i != -1; last = i, i = nexts[i])
+				if (ec.Equals(keys[i], key))
+				{
+					if (update) values[i] = value;
+					return update;
+				}
+
+			i = last == -1 ? h : (last + 1) & f;
+			while (u[i]) i = (i + 1) & f;
+
+			if (last == -1) firsts[h] = i;
+			else nexts[last] = i;
+			u[i] = true;
+			keys[i] = key;
+			values[i] = value;
+			++n;
+			return true;
+		}
+
+		public bool Remove(TKey key)
+		{
+			var h = key.GetHashCode() & f;
+			var last = -1;
+			for (var i = firsts[h]; i != -1; last = i, i = nexts[i])
+			{
+				if (!ec.Equals(keys[i], key)) continue;
+
+				if (last == -1) firsts[h] = nexts[i];
+				else nexts[last] = nexts[i];
+				nexts[i] = -1;
+				u[i] = false;
+				--n;
+				return true;
+			}
+			return false;
+		}
+
+		void Expand()
+		{
+			var (tu, tk, tv) = (u, keys, values);
+			Initialize(u.Length << 1);
+			for (var i = 0; i < tu.Length; ++i) if (tu[i]) AddOrUpdate(tk[i], tv[i], false);
+		}
+
+		System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator() => GetEnumerator();
+		public IEnumerator<(TKey key, TValue value)> GetEnumerator() { for (var i = 0; i <= f; ++i) if (u[i]) yield return (keys[i], values[i]); }
+	}
+}
