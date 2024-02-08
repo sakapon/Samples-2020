e4870546f67ccb9e7699bd3e400b65c8395275fc
Keiho Sakapon <git@saka-pon.net>
2024-02-09 00:36:04 +0900

    Add Int32TrieSet class

---
+++ AlgorithmSample/AlgorithmLab/DataTrees/Tries/Int32TrieSet103.cs
@@ -0,0 +1,135 @@
+﻿using System;
+
+namespace AlgorithmLab.DataTrees.Tries.Tries103
+{
+	[System.Diagnostics.DebuggerDisplay(@"Count = {Count}")]
+	public class Int32TrieSet
+	{
+		[System.Diagnostics.DebuggerDisplay(@"Key = {Key}, Enabled = {Enabled}")]
+		public class Node
+		{
+			public int Key;
+			public bool Enabled;
+			public Node Left;
+			public Node Right;
+		}
+
+		Node Root = new Node();
+		public long Count;
+
+		public void Clear()
+		{
+			Root = new Node();
+			Count = 0;
+		}
+
+		Node GetNode(int item)
+		{
+			var node = Root;
+			while (node != null)
+			{
+				if (item == node.Key) break;
+				if (item < node.Key) node = node.Left;
+				else node = node.Right;
+			}
+			return node;
+		}
+
+		public bool Contains(int item)
+		{
+			var node = GetNode(item);
+			return node != null && node.Enabled;
+		}
+
+		public bool Remove(int item)
+		{
+			var node = GetNode(item);
+			if (node == null || !node.Enabled) return false;
+
+			node.Enabled = false;
+			--Count;
+			return true;
+		}
+
+		public bool Add(int item)
+		{
+			Node node = Root, p = null;
+			while (true)
+			{
+				if (item == node.Key) break;
+
+				var lca = GetLca(item, node.Key);
+				if (lca == node.Key)
+				{
+					p = node;
+					if (item < node.Key)
+					{
+						if (node.Left == null) node.Left = new Node { Key = item };
+						node = node.Left;
+					}
+					else
+					{
+						if (node.Right == null) node.Right = new Node { Key = item };
+						node = node.Right;
+					}
+				}
+				else if (lca == item)
+				{
+					var mn = new Node { Key = item };
+
+					if (item < p.Key) p.Left = mn;
+					else p.Right = mn;
+
+					if (node.Key < item) mn.Left = node;
+					else mn.Right = node;
+
+					node = mn;
+					break;
+				}
+				else
+				{
+					var mn = new Node { Key = lca };
+
+					if (lca < p.Key) p.Left = mn;
+					else p.Right = mn;
+
+					if (node.Key < lca)
+					{
+						mn.Left = node;
+						node = mn.Right = new Node { Key = item };
+					}
+					else
+					{
+						mn.Right = node;
+						node = mn.Left = new Node { Key = item };
+					}
+					break;
+				}
+			}
+			if (node.Enabled) return false;
+
+			node.Enabled = true;
+			++Count;
+			return true;
+		}
+
+		static int GetLca(int x, int y)
+		{
+			if (x == 0 || y == 0) return 0;
+			if ((-x & x) < (-y & y)) (x, y) = (y, x);
+			if (x - (-x & x) < y && y < x + (-x & x)) return x;
+			var f = MaxBit(x ^ y);
+			return x & ~(f - 1) | f;
+		}
+
+		static int MaxBit(int x)
+		{
+			x |= x >> 1;
+			x |= x >> 2;
+			x |= x >> 4;
+			x |= x >> 8;
+			x |= x >> 16;
+			return x ^ (x >> 1);
+		}
+	}
+}
