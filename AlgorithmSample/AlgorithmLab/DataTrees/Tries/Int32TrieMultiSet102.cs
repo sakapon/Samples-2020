d93a561badad614763c00bc07ebbc601252fe1be
Keiho Sakapon <git@saka-pon.net>
2024-02-08 18:19:52 +0900

    Add class for trie set

---
+++ AlgorithmSample/AlgorithmLab/DataTrees/Tries/Int32TrieMultiSet102.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace AlgorithmLab.DataTrees.Tries.Tries102
+{
+	[System.Diagnostics.DebuggerDisplay(@"Count = {Count}")]
+	public class Int32TrieMultiSet
+	{
+		[System.Diagnostics.DebuggerDisplay(@"Count = {Count}")]
+		public class Node
+		{
+			public long Count;
+			public Node Left;
+			public Node Right;
+		}
+
+		const int MaxDigit = 30;
+		readonly Node Root = new Node();
+		public long Count;
+
+		public long GetCount(int item)
+		{
+			var node = Root;
+			for (var f = 1 << MaxDigit; f != 0 && item != 0; f >>= 1)
+			{
+				if (item < 0)
+				{
+					if ((node = node.Left) == null) return 0;
+					item += f;
+				}
+				else
+				{
+					if ((node = node.Right) == null) return 0;
+					item -= f;
+				}
+			}
+			return node.Count;
+		}
+
+		public void Add(int item, long count = 1)
+		{
+			Count += count;
+			var node = Root;
+			for (var f = 1 << MaxDigit; f != 0 && item != 0; f >>= 1)
+			{
+				if (item < 0)
+				{
+					node = node.Left ??= new Node();
+					item += f;
+				}
+				else
+				{
+					node = node.Right ??= new Node();
+					item -= f;
+				}
+			}
+			node.Count += count;
+		}
+	}
+}
---
+++ AlgorithmSample/AlgorithmLab/DataTrees/Tries/Int32TrieSet102.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace AlgorithmLab.DataTrees.Tries.Tries102
+{
+	[System.Diagnostics.DebuggerDisplay(@"Count = {Count}")]
+	public class Int32TrieSet
+	{
+		public class Node
+		{
+			public bool Enabled;
+			public Node Left;
+			public Node Right;
+		}
+
+		const int MaxDigit = 30;
+		readonly Node Root = new Node();
+		public long Count;
+
+		Node GetNode(int item)
+		{
+			var node = Root;
+			for (var f = 1 << MaxDigit; f != 0 && item != 0; f >>= 1)
+			{
+				if (item < 0)
+				{
+					if ((node = node.Left) == null) break;
+					item += f;
+				}
+				else
+				{
+					if ((node = node.Right) == null) break;
+					item -= f;
+				}
+			}
+			return node;
+		}
+
+		public bool Contains(int item)
+		{
+			var node = GetNode(item);
+			return node != null && node.Enabled;
+		}
+
+		public bool Add(int item)
+		{
+			var node = Root;
+			for (var f = 1 << MaxDigit; f != 0 && item != 0; f >>= 1)
+			{
+				if (item < 0)
+				{
+					node = node.Left ??= new Node();
+					item += f;
+				}
+				else
+				{
+					node = node.Right ??= new Node();
+					item -= f;
+				}
+			}
+			if (node.Enabled) return false;
+			node.Enabled = true;
+			++Count;
+			return true;
+		}
+
+		public bool Remove(int item)
+		{
+			var node = GetNode(item);
+			if (node == null || !node.Enabled) return false;
+			node.Enabled = false;
+			--Count;
+			return true;
+		}
+	}
+}
