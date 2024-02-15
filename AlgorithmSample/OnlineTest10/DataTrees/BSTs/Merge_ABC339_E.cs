e4c128847628a13df0eae5136f117c6321eca459
Keiho Sakapon <git@saka-pon.net>
2024-02-15 09:25:52 +0900

    Fix CRLF

---
+++ AlgorithmSample/OnlineTest10/DataTrees/BSTs/Merge_ABC339_E.cs
@@ -0,0 +1,26 @@
+﻿using AlgorithmLib10.DataTrees.BSTs.BSTs203;
+
+namespace OnlineTest10.DataTrees.BSTs
+{
+	// Test: https://atcoder.jp/contests/abc339/tasks/abc339_e
+	class Merge_ABC339_E
+	{
+		static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+		static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
+		static void Main() => Console.WriteLine(Solve());
+		static object Solve()
+		{
+			var (n, d) = Read2();
+			var a = Read();
+
+			var monoid = new Monoid<int>((x, y) => x >= y ? x : y, 0);
+			var st = new Int32LCATreeMerge<int>(monoid);
+
+			foreach (var v in a)
+			{
+				st[v] = Math.Max(st[v], st[v - d, v + d + 1] + 1);
+			}
+			return st[0, 1 << 30];
+		}
+	}
+}
