3c93258b2c1bed02272208f88df227927e29a4e9
Keiho Sakapon <git@saka-pon.net>
2024-02-19 09:43:03 +0900

    Use center of segment as key

--- AlgorithmSample/AlgorithmLib10/DataTrees/BSTs204/Int32LCATreeRSQ.cs
+++ AlgorithmSample/AlgorithmLib10/DataTrees/BSTs204/Int32LCATreeRSQ.cs
@@ -3,13 +3,13 @@ namespace AlgorithmLib10.DataTrees.BSTs.BSTs204
 {
 	public class Int32LCATreeRSQ
 	{
-		// 区間を表します。
+		// ノードは区間を表します。
 		[System.Diagnostics.DebuggerDisplay(@"Key = {Key}, Value = {Value}")]
 		public class Node
 		{
-			// 区間の左端
+			// 区間の中央
 			public int Key;
-			// 区間の長さの半分
+			// 区間の長さの半分 (葉の場合は 0)
 			public int HalfLength;
 			public long Value;
 			public Node Left, Right;
@@ -17,40 +17,35 @@ namespace AlgorithmLib10.DataTrees.BSTs.BSTs204
 
 		// [-1 << MaxDigit, 1 << MaxDigit)
 		const int MaxDigit = 30;
-		Node Root = new Node { Key = -1 << MaxDigit, HalfLength = 1 << MaxDigit };
+		Node Root;
 		readonly List<Node> Path = new List<Node>();
 
-		public void Clear() => Root = new Node { Key = -1 << MaxDigit, HalfLength = MaxDigit };
+		public Int32LCATreeRSQ() => Clear();
+		public void Clear() => Root = new Node { HalfLength = 1 << MaxDigit };
 
-		public long this[int key] => this[key, key + 1];
-		public long this[int l, int r]
-		{
-			get
-			{
-				ScanNode(l, r);
-				var v = 0L;
-				foreach (var n in Path) v += n.Value;
-				return v;
-			}
-		}
+		public long this[int key] => Get(key, key + 1);
+		public long this[int l, int r] => Get(l, r);
 
 		public void Add(int key, long value)
 		{
-			var node = GetOrAddNode(key);
+			GetOrAddNode(key);
 			foreach (var n in Path) n.Value += value;
 		}
 
-		void ScanNode(int l, int r)
+		long Get(int l, int r)
 		{
 			Path.Clear();
 			ScanNode(Root, l, r);
+			var v = 0L;
+			foreach (var n in Path) v += n.Value;
+			return v;
 		}
 
 		void ScanNode(Node node, int l, int r)
 		{
 			if (node == null) return;
-			if (l <= node.Key && node.Key + (node.HalfLength << 1) <= r) { Path.Add(node); return; }
-			var nc = node.Key + node.HalfLength;
+			var nc = node.Key;
+			if (l <= nc - node.HalfLength && nc + node.HalfLength <= r) { Path.Add(node); return; }
 			if (l < nc) ScanNode(node.Left, l, nc < r ? nc : r);
 			if (nc < r) ScanNode(node.Right, l < nc ? nc : l, r);
 		}
@@ -63,7 +58,7 @@ namespace AlgorithmLib10.DataTrees.BSTs.BSTs204
 			{
 				if (node == null) { node = new Node { Key = key }; break; }
 
-				var d = key.CompareTo(node.Key + node.HalfLength);
+				var d = key.CompareTo(node.Key);
 				if (d == 0 && node.HalfLength == 0) break;
 
 				var lca = GetLca(node, key);
@@ -86,11 +81,11 @@ namespace AlgorithmLib10.DataTrees.BSTs.BSTs204
 
 		static Node GetLca(Node node, int key)
 		{
-			var l = node.Key;
-			if (l <= key && key < l + (node.HalfLength << 1)) return null;
-			var f = MaxBit(l ^ key);
-			var lca = new Node { Key = l & ~(f | (f - 1)), HalfLength = f, Value = node.Value };
-			(l < lca.Key + lca.HalfLength ? ref lca.Left : ref lca.Right) = node;
+			var nc = node.Key;
+			if (nc - node.HalfLength <= key && key < nc + node.HalfLength) return null;
+			var f = MaxBit(nc ^ key);
+			var lca = new Node { Key = key & ~(f - 1) | f, HalfLength = f, Value = node.Value };
+			(nc - node.HalfLength < lca.Key ? ref lca.Left : ref lca.Right) = node;
 			return lca;
 		}
 
