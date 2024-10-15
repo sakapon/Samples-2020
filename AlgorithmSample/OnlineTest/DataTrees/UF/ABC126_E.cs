0248c087c582fad53e27211d0d196d530048993d
sakapon <git@saka-pon.net>
2024-10-15 10:47:00 +0900

    Add test

---
+++ AlgorithmSample/OnlineTest/DataTrees/UF/ABC126_E.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using AlgorithmLab.DataTrees.UF401;
+
+namespace OnlineTest.DataTrees.UF
+{
+	// Test: https://atcoder.jp/contests/abc126/tasks/abc126_e
+	class ABC126_E
+	{
+		static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+		static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
+		static (int, int, int) Read3() { var a = Read(); return (a[0], a[1], a[2]); }
+		static void Main() => Console.WriteLine(Solve());
+		static object Solve()
+		{
+			var (n, m) = Read2();
+			var es = Array.ConvertAll(new bool[m], _ => Read3());
+
+			var uf = new UnionFind(n + 1);
+			foreach (var (x, y, _) in es)
+				uf.Union(x, y);
+			return uf.SetsCount - 1;
+		}
+	}
+}
