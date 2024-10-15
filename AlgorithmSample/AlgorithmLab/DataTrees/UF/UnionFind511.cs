fec8629d7ff0c7231c704babcc675b3dfb458a7e
sakapon <git@saka-pon.net>
2024-10-15 12:16:45 +0900

    Add UnionFind 511

--- AlgorithmSample/AlgorithmLab/DataTrees/UF/UnionFind511.cs
+++ AlgorithmSample/AlgorithmLab/DataTrees/UF/UnionFind511.cs
@@ -1,11 +1,12 @@
 ﻿using System;
 using System.Linq;
 
-// Int32 vertexes, node-based
-namespace AlgorithmLab.DataTrees.UF611
+// 実用可能です。
+// Int32 vertexes, node-based, data augmentation
+namespace AlgorithmLab.DataTrees.UF511
 {
 	[System.Diagnostics.DebuggerDisplay(@"ItemsCount = {ItemsCount}, SetsCount = {SetsCount}")]
-	public class UnionFind
+	public class UnionFind<TValue>
 	{
 		[System.Diagnostics.DebuggerDisplay(@"\{{Item}, {Value}\}")]
 		public class Node
@@ -13,8 +14,7 @@ namespace AlgorithmLab.DataTrees.UF611
 			public int Item;
 			public Node Parent;
 			public int Size = 1;
-			// 親を基準とした相対値
-			public long Value;
+			public TValue Value;
 		}
 
 		readonly Node[] nodes;
@@ -22,43 +22,36 @@ namespace AlgorithmLab.DataTrees.UF611
 		public Node this[int x] { get { Find(x); return nodes[x]; } }
 		public int ItemsCount => nodes.Length;
 		public int SetsCount { get; private set; }
+		public Func<TValue, TValue, TValue> MergeValues { get; }
 
-		public UnionFind(int n)
+		public UnionFind(int n, Func<TValue, TValue, TValue> mergeValues, TValue[] values = null)
 		{
+			MergeValues = mergeValues;
+			values ??= new TValue[n];
 			nodes = new Node[n];
-			for (int i = 0; i < n; ++i) nodes[i] = new Node { Item = i };
+			for (int i = 0; i < n; ++i) nodes[i] = new Node { Item = i, Value = values[i] };
 			SetsCount = n;
 		}
 
-		Node Find(Node n)
-		{
-			if (n.Parent == null) return n;
-
-			var r = Find(n.Parent);
-			n.Value += n.Parent.Value;
-			return n.Parent = r;
-		}
-
+		Node Find(Node n) => n.Parent == null ? n : n.Parent = Find(n.Parent);
 		public Node Find(int x) => Find(nodes[x]);
 		public bool AreSame(int x, int y) => Find(x) == Find(y);
 		public int GetSize(int x) => Find(x).Size;
 
-		public bool Union(int x, int y, long x2y)
+		public bool Union(int x, int y)
 		{
 			var rx = Find(x);
 			var ry = Find(y);
 			if (rx == ry) return false;
 
-			if (rx.Size < ry.Size)
-			{
-				(x, y) = (y, x);
-				(rx, ry) = (ry, rx);
-				x2y = -x2y;
-			}
+			// 左右の順序を保って値をマージします。
+			var v = MergeValues(rx.Value, ry.Value);
+
+			if (rx.Size < ry.Size) (rx, ry) = (ry, rx);
 			ry.Parent = rx;
 			rx.Size += ry.Size;
 			--SetsCount;
-			ry.Value = nodes[x].Value - nodes[y].Value + x2y;
+			rx.Value = v;
 			return true;
 		}
 
---
+++ AlgorithmSample/OnlineTest/DataTrees/UF/ABC372_E.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using AlgorithmLab.DataTrees.UF511;
+
+namespace OnlineTest.DataTrees.UF
+{
+	// Test: https://atcoder.jp/contests/abc372/tasks/abc372_e
+	class ABC372_E
+	{
+		static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+		static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
+		static (int, int, int) Read3() { var a = Read(); return (a[0], a[1], a[2]); }
+		static void Main() => Console.WriteLine(Solve());
+		static object Solve()
+		{
+			var (n, qc) = Read2();
+			var qs = Array.ConvertAll(new bool[qc], _ => Read3());
+
+			var r = new List<int>();
+			var values = Enumerable.Range(0, n + 1).Select(i => new List<int> { i }).ToArray();
+			var uf = new UnionFind<List<int>>(n + 1, (a, b) =>
+			{
+				a.AddRange(b);
+				a.Sort();
+				a.Reverse();
+				while (a.Count > 10) a.RemoveAt(a.Count - 1);
+				return a;
+			}, values);
+
+			foreach (var (t, u, v) in qs)
+			{
+				if (t == 1)
+				{
+					uf.Union(u, v);
+				}
+				else
+				{
+					var l = uf.Find(u).Value;
+					r.Add(l.Count >= v ? l[v - 1] : -1);
+				}
+			}
+			return string.Join("\n", r);
+		}
+	}
+}
