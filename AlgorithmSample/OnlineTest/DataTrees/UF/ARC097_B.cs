2d444be6cbb42dc23409a41e4776ab94d80f0979
sakapon <git@saka-pon.net>
2024-10-17 10:03:15 +0900

    Add test

---
+++ AlgorithmSample/OnlineTest/DataTrees/UF/ARC097_B.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using AlgorithmLab.DataTrees.UF401;
+
+namespace OnlineTest.DataTrees.UF
+{
+	// Test: https://atcoder.jp/contests/abc097/tasks/arc097_b
+	class ARC097_B
+	{
+		static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+		static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
+		static void Main() => Console.WriteLine(Solve());
+		static object Solve()
+		{
+			var (n, m) = Read2();
+			var p = Read();
+			var es = Array.ConvertAll(new bool[m], _ => Read2());
+
+			var uf = new UnionFind(n + 1);
+			foreach (var (x, y) in es)
+			{
+				uf.Union(x, y);
+			}
+			return Enumerable.Range(0, n).Count(i => uf.AreSame(i + 1, p[i]));
+		}
+	}
+}
