29e44929bde1e51edfa0fcee1060732652fb99b5
Keiho Sakapon <git@saka-pon.net>
2024-10-19 22:18:33 +0900

    Add UnionFind 202

---
+++ AlgorithmSample/AlgorithmLab/DataTrees/UF/UnionFind202.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+// path compression
+// O(log n)
+namespace AlgorithmLab.DataTrees.UF202
+{
+	public class UnionFind
+	{
+		readonly int[] p;
+		public UnionFind(int n) => p = Enumerable.Repeat(-1, n).ToArray();
+		public int Find(int x) => p[x] == -1 ? x : p[x] = Find(p[x]);
+		public bool AreSame(int x, int y) => Find(x) == Find(y);
+		public void Union(int x, int y) { if ((x = Find(x)) != (y = Find(y))) p[y] = x; }
+	}
+}
