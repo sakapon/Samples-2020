db7e67efe56889c09e871096e181cac531f63191
Keiho Sakapon <git@saka-pon.net>
2024-10-22 00:16:51 +0900

    Add test

---
+++ AlgorithmSample/OnlineTest/DataTrees/UF/Int3/ABC214_D.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using AlgorithmLab.DataTrees.UF401;
+
+namespace OnlineTest.DataTrees.UF
+{
+	// Test: https://atcoder.jp/contests/abc214/tasks/abc214_d
+	class ABC214_D
+	{
+		static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+		static (int, int, int w) Read3() { var a = Read(); return (a[0], a[1], a[2]); }
+		static void Main() => Console.WriteLine(Solve());
+		static object Solve()
+		{
+			var n = int.Parse(Console.ReadLine());
+			var es = Array.ConvertAll(new bool[n - 1], _ => Read3());
+
+			var r = 0L;
+			var uf = new UnionFind(n + 1);
+
+			foreach (var (u, v, w) in es.OrderBy(e => e.w))
+			{
+				r += (long)w * uf.GetSize(u) * uf.GetSize(v);
+				uf.Union(u, v);
+			}
+			return r;
+		}
+	}
+}
---
+++ AlgorithmSample/OnlineTest/DataTrees/UF/Int4/APC001_D.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using AlgorithmLab.DataTrees.UF401;
+
+namespace OnlineTest.DataTrees.UF
+{
+	// Test: https://atcoder.jp/contests/apc001/tasks/apc001_d
+	class APC001_D
+	{
+		static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+		static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
+		static void Main() => Console.WriteLine(Solve());
+		static object Solve()
+		{
+			var (n, m) = Read2();
+			var a = Read();
+			var es = Array.ConvertAll(new bool[m], _ => Read2());
+
+			var uf = new UnionFind(n);
+			foreach (var (x, y) in es)
+				uf.Union(x, y);
+
+			// uf.SetsCount == n - m
+			if (n - m - 1 == 0) return 0;
+			if (2 * (n - m - 1) > n) return "Impossible";
+
+			var r = 0L;
+			var cl2 = new List<int>();
+
+			foreach (var g in uf.ToSets())
+			{
+				var cs = g.Select(v => a[v]).ToArray();
+				Array.Sort(cs);
+				r += cs[0];
+				cl2.AddRange(cs[1..]);
+			}
+
+			var cs2 = cl2.ToArray();
+			Array.Sort(cs2);
+			r += cs2[..(n - m - 2)].Sum(c => (long)c);
+			return r;
+		}
+	}
+}
