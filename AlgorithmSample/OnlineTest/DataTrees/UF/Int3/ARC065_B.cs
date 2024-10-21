53e0e62e460adf71d091d722070ae58927708f8b
Keiho Sakapon <git@saka-pon.net>
2024-10-21 15:19:51 +0900

    Add test

---
+++ AlgorithmSample/OnlineTest/DataTrees/UF/Int3/ARC065_B.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using AlgorithmLab.DataTrees.UF401;
+
+namespace OnlineTest.DataTrees.UF
+{
+	// Test: https://atcoder.jp/contests/abc049/tasks/arc065_b
+	class ARC065_B
+	{
+		static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+		static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
+		static (int, int, int) Read3() { var a = Read(); return (a[0], a[1], a[2]); }
+		static void Main() => Console.WriteLine(Solve());
+		static object Solve()
+		{
+			var (n, k, l) = Read3();
+			var es1 = Array.ConvertAll(new bool[k], _ => Read2());
+			var es2 = Array.ConvertAll(new bool[l], _ => Read2());
+
+			var uf1 = new UnionFind(n + 1);
+			foreach (var (u, v) in es1)
+				uf1.Union(u, v);
+
+			var uf2 = new UnionFind(n + 1);
+			foreach (var (u, v) in es2)
+				uf2.Union(u, v);
+
+			var r = new int[n + 1];
+			var q = Enumerable.Range(0, n + 1).GroupBy(v => (uf1.Find(v), uf2.Find(v)));
+
+			foreach (var g in q)
+			{
+				var c = g.Count();
+				foreach (var v in g)
+					r[v] = c;
+			}
+
+			return string.Join(" ", r[1..]);
+		}
+	}
+}
---
+++ AlgorithmSample/OnlineTest/DataTrees/UF/Int4/ABC247_F.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using AlgorithmLab.DataTrees.UF401;
+
+namespace OnlineTest.DataTrees.UF
+{
+	// Test: https://atcoder.jp/contests/abc247/tasks/abc247_f
+	class ABC247_F
+	{
+		const long M = 998244353;
+		static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+		static void Main() => Console.WriteLine(Solve());
+		static object Solve()
+		{
+			var n = int.Parse(Console.ReadLine());
+			var p = Read();
+			var q = Read();
+
+			var uf = new UnionFind(n + 1);
+			for (int i = 0; i < n; i++)
+				uf.Union(p[i], q[i]);
+
+			var f = new long[n + 1];
+			f[0] = 2;
+			f[1] = 1;
+			for (int i = 2; i <= n; i++)
+				f[i] = (f[i - 1] + f[i - 2]) % M;
+
+			return uf.ToSets().Select(g => f[g.Count()]).Aggregate((x, y) => x * y % M);
+		}
+	}
+}
