1e8c2a6ddc4db3fc52cd514b2b815759bf6a5c62
Keiho Sakapon <git@saka-pon.net>
2024-10-27 19:42:55 +0900

    Add test

---
+++ AlgorithmSample/OnlineTest/DataTrees/UF/ARC036_D.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnionFind1 = AlgorithmLab.DataTrees.UF511.UnionFind<bool>;
+using UnionFind2 = AlgorithmLab.DataTrees.UF602.UnionFind<bool>;
+
+namespace OnlineTest.DataTrees.UF
+{
+	// Test: https://atcoder.jp/contests/arc036/tasks/arc036_d
+	class ARC036_D
+	{
+		static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+		static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
+		static (int, int, int, int) Read4() { var a = Read(); return (a[0], a[1], a[2], a[3]); }
+		static void Main() => Console.WriteLine(Solve());
+		static object Solve()
+		{
+			var (n, qc) = Read2();
+			var qs = Array.ConvertAll(new bool[qc], _ => Read4());
+
+			var r = new List<bool>();
+			var uf1 = new UnionFind1(n + 1, (x, y) => x | y);
+			var uf2 = new UnionFind2(n + 1, false, x => x, (x, y) => x ^ y);
+
+			foreach (var (w, x, y, z) in qs)
+			{
+				if (w == 1)
+				{
+					uf1.Union(x, y);
+					if (uf1.GetValue(x)) continue;
+
+					if (!uf2.Union(x, y, z % 2 != 0) && !uf2.Verify(x, y, z % 2 != 0))
+					{
+						uf1.Find(x).Value = true;
+					}
+				}
+				else
+				{
+					r.Add(uf1.AreSame(x, y) && (uf1.GetValue(x) || uf2.Verify(x, y, false)));
+				}
+			}
+			return string.Join("\n", r.Select(b => b ? "YES" : "NO"));
+		}
+	}
+}
---
+++ AlgorithmSample/OnlineTest/DataTrees/UF/Int4/ARC036_D2.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using AlgorithmLab.DataTrees.UF401;
+
+namespace OnlineTest.DataTrees.UF
+{
+	// Test: https://atcoder.jp/contests/arc036/tasks/arc036_d
+	class ARC036_D2
+	{
+		static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+		static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
+		static (int, int, int, int) Read4() { var a = Read(); return (a[0], a[1], a[2], a[3]); }
+		static void Main() => Console.WriteLine(Solve());
+		static object Solve()
+		{
+			var (n, qc) = Read2();
+			var qs = Array.ConvertAll(new bool[qc], _ => Read4());
+
+			var r = new List<bool>();
+			var uf = new UnionFind(2 * n + 1);
+
+			foreach (var (w, x, y, z) in qs)
+			{
+				if (w == 1)
+				{
+					if (z % 2 == 0)
+					{
+						uf.Union(x, y);
+						uf.Union(x + n, y + n);
+					}
+					else
+					{
+						uf.Union(x, y + n);
+						uf.Union(x + n, y);
+					}
+				}
+				else
+				{
+					r.Add(uf.AreSame(x, y));
+				}
+			}
+			return string.Join("\n", r.Select(b => b ? "YES" : "NO"));
+		}
+	}
+}
