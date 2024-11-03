b52ec6e744325b657aa4878737c124fa0ad230dd
Keiho Sakapon <git@saka-pon.net>
2024-11-03 23:48:02 +0900

    Add test

---
+++ AlgorithmSample/OnlineTest/DataTrees/UF/Int4/ABC378_F.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using AlgorithmLab.DataTrees.UF401;
+
+namespace OnlineTest.DataTrees.UF
+{
+	// Test: https://atcoder.jp/contests/abc378/tasks/abc378_f
+	class ABC378_F
+	{
+		static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+		static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
+		static void Main() => Console.WriteLine(Solve());
+		static object Solve()
+		{
+			var n = int.Parse(Console.ReadLine());
+			var es = Array.ConvertAll(new bool[n - 1], _ => Read2());
+
+			var map = Array.ConvertAll(new bool[n + 1], _ => new List<int>());
+			foreach (var (u, v) in es)
+			{
+				map[u].Add(v);
+				map[v].Add(u);
+			}
+
+			var r = 0L;
+			var uf = new UnionFind(n + 1);
+
+			foreach (var (u, v) in es)
+			{
+				if (map[u].Count == 3 && map[v].Count == 3)
+					uf.Union(u, v);
+			}
+
+			foreach (var g in uf.ToSets())
+			{
+				if (map[g.Key].Count != 3) continue;
+
+				var c = 0L;
+				foreach (var v in g)
+				{
+					foreach (var nv in map[v])
+					{
+						if (map[nv].Count == 2) c++;
+					}
+				}
+				r += c * (c - 1) / 2;
+			}
+			return r;
+		}
+	}
+}
