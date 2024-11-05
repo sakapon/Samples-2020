727eb7e423e1b66c8574fd93100fa1379a3cbe46
Keiho Sakapon <git@saka-pon.net>
2024-11-05 22:04:20 +0900

    Add test

---
+++ AlgorithmSample/OnlineTest/DataTrees/UF/Int3/ABC293_D.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using AlgorithmLab.DataTrees.UF401;
+
+namespace OnlineTest.DataTrees.UF
+{
+	// Test: https://atcoder.jp/contests/abc293/tasks/abc293_d
+	class ABC293_D
+	{
+		static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+		static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
+		static void Main() => Console.WriteLine(Solve());
+		static object Solve()
+		{
+			var (n, m) = Read2();
+			var es = Array.ConvertAll(new bool[m], _ => Console.ReadLine().Split());
+
+			var uf = new UnionFind(n + 1);
+			var c = 0;
+
+			foreach (var e in es)
+			{
+				var u = int.Parse(e[0]);
+				var v = int.Parse(e[2]);
+
+				if (!uf.Union(u, v)) c++;
+			}
+			return $"{c} {uf.SetsCount - c - 1}";
+		}
+	}
+}
---
+++ AlgorithmSample/OnlineTest/DataTrees/UF/Int3/ABC333_D.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using AlgorithmLab.DataTrees.UF411;
+
+namespace OnlineTest.DataTrees.UF
+{
+	// Test: https://atcoder.jp/contests/abc333/tasks/abc333_d
+	class ABC333_D
+	{
+		static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+		static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
+		static void Main() => Console.WriteLine(Solve());
+		static object Solve()
+		{
+			var n = int.Parse(Console.ReadLine());
+			var es = Array.ConvertAll(new bool[n - 1], _ => Read2());
+
+			var uf = new UnionFind(n + 1);
+
+			foreach (var (u, v) in es)
+			{
+				if (u != 1)
+					uf.Union(u, v);
+			}
+			return n - uf.GetSetInfoes().Max(g => g.Size);
+		}
+	}
+}
