963ecc1dd4b3adb51dee949b210a0c9c0bcc9567
Keiho Sakapon <git@saka-pon.net>
2024-11-05 22:35:49 +0900

    Add test

---
+++ AlgorithmSample/OnlineTest/DataTrees/UF/Grid/AIS19_C.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using AlgorithmLab.DataTrees.UF511;
+
+namespace OnlineTest.DataTrees.UF
+{
+	// Test: https://atcoder.jp/contests/aising2019/tasks/aising2019_c
+	class AIS19_C
+	{
+		static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+		static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
+		static void Main() => Console.WriteLine(Solve());
+		static object Solve()
+		{
+			var (h, w) = Read2();
+			var s0 = Array.ConvertAll(new bool[h], _ => Console.ReadLine());
+
+			var n = h * w;
+			var p = s0.SelectMany(t => t.Select(c => c == '#')).ToArray();
+
+			var iv = Array.ConvertAll(p, b => b ? (1, 0) : (0, 1));
+			var uf = new UnionFind<(int c, int d)>(n, (a, b) => (a.c + b.c, a.d + b.d), iv);
+
+			for (int i = 0; i < h; i++)
+				for (int j = 1; j < w; j++)
+				{
+					var v = w * i + j;
+					if (p[v] != p[v - 1]) uf.Union(v, v - 1);
+				}
+
+			for (int j = 0; j < w; j++)
+				for (int i = 1; i < h; i++)
+				{
+					var v = w * i + j;
+					if (p[v] != p[v - w]) uf.Union(v, v - w);
+				}
+
+			return uf.ToSets().Select(g => g.Key.Value).Sum(p => (long)p.c * p.d);
+		}
+	}
+}
