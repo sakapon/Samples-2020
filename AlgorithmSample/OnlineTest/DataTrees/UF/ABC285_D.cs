6a5ff62969b922ef76dcd272c8bd4ca6308e6fe3
Keiho Sakapon <git@saka-pon.net>
2024-11-06 21:42:52 +0900

    Add test

---
+++ AlgorithmSample/OnlineTest/DataTrees/UF/ABC285_D.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using AlgorithmLab.DataTrees.UF402;
+
+namespace OnlineTest.DataTrees.UF
+{
+	// Test: https://atcoder.jp/contests/abc285/tasks/abc285_d
+	class ABC285_D
+	{
+		static void Main() => Console.WriteLine(Solve() ? "Yes" : "No");
+		static bool Solve()
+		{
+			var n = int.Parse(Console.ReadLine());
+			var es = Array.ConvertAll(new bool[n], _ => Console.ReadLine().Split());
+
+			var uf = new UnionFind<string>();
+
+			foreach (var e in es)
+			{
+				if (!uf.Union(e[0], e[1])) return false;
+			}
+			return true;
+		}
+	}
+}
