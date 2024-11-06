fad18a27dfca5bad644fa7fdcc840dcf623e7eec
Keiho Sakapon <git@saka-pon.net>
2024-11-06 21:17:38 +0900

    Add test

--- AlgorithmSample/OnlineTest/DataTrees/UF/MST/ABC218_E.cs
+++ AlgorithmSample/OnlineTest/DataTrees/UF/MST/ABC218_E.cs
@@ -5,26 +5,24 @@ using AlgorithmLab.DataTrees.UF401;
 
 namespace OnlineTest.DataTrees.UF
 {
-	// Test: https://atcoder.jp/contests/arc029/tasks/arc029_3
-	class ARC029_C
+	// Test: https://atcoder.jp/contests/abc218/tasks/abc218_e
+	class ABC218_E
 	{
 		static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
 		static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
-		static (int, int, int w) Read3() { var a = Read(); return (a[0], a[1], a[2]); }
+		static (int, int, int c) Read3() { var a = Read(); return (a[0], a[1], a[2]); }
 		static void Main() => Console.WriteLine(Solve());
 		static object Solve()
 		{
 			var (n, m) = Read2();
-			var cs = Array.ConvertAll(new bool[n], _ => int.Parse(Console.ReadLine()));
 			var es = Array.ConvertAll(new bool[m], _ => Read3());
 
 			var r = 0L;
 			var uf = new UnionFind(n + 1);
-			var cs2 = Enumerable.Range(0, n).Select(i => (0, i + 1, w: cs[i]));
 
-			foreach (var (u, v, w) in cs2.Concat(es).OrderBy(p => p.w))
+			foreach (var (u, v, c) in es.OrderBy(e => e.c))
 			{
-				if (uf.Union(u, v)) r += w;
+				if (!uf.Union(u, v) && c >= 0) r += c;
 			}
 			return r;
 		}
---
+++ AlgorithmSample/OnlineTest/DataTrees/UF/MST/ABC282_E.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using AlgorithmLab.DataTrees.UF401;
+
+namespace OnlineTest.DataTrees.UF
+{
+	// Test: https://atcoder.jp/contests/abc282/tasks/abc282_e
+	class ABC282_E
+	{
+		static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+		static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
+		static void Main() => Console.WriteLine(Solve());
+		static object Solve()
+		{
+			var (n, m) = Read2();
+			var a = Read();
+
+			long GetScore(int x, int y) => (MPow(x, y, m) + MPow(y, x, m)) % m;
+			var es = new List<(int, int, long c)>();
+
+			for (int i = 0; i < n; i++)
+				for (int j = i + 1; j < n; j++)
+					es.Add((i, j, GetScore(a[i], a[j])));
+
+			var r = 0L;
+			var uf = new UnionFind(n);
+
+			foreach (var (u, v, c) in es.OrderBy(e => -e.c))
+			{
+				if (uf.Union(u, v)) r += c;
+			}
+			return r;
+		}
+
+		static long MPow(long b, long i, long M)
+		{
+			long r = 1;
+			for (; i != 0; b = b * b % M, i >>= 1) if ((i & 1) != 0) r = r * b % M;
+			return r;
+		}
+	}
+}
