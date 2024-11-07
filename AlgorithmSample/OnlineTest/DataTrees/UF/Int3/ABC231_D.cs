48219c73691e1df178eed1d9ec2b07e1342dc8a0
Keiho Sakapon <git@saka-pon.net>
2024-11-07 22:04:25 +0900

    Add test

---
+++ AlgorithmSample/OnlineTest/DataTrees/UF/Int3/ABC231_D.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using AlgorithmLab.DataTrees.UF401;
+
+namespace OnlineTest.DataTrees.UF
+{
+	// Test: https://atcoder.jp/contests/abc231/tasks/abc231_d
+	class ABC231_D
+	{
+		static int[] Read() => Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+		static (int, int) Read2() { var a = Read(); return (a[0], a[1]); }
+		static void Main() => Console.WriteLine(Solve() ? "Yes" : "No");
+		static bool Solve()
+		{
+			var (n, m) = Read2();
+			var es = Array.ConvertAll(new bool[m], _ => Read2());
+
+			var uf = new UnionFind(n + 1);
+			var degs = new int[n + 1];
+
+			foreach (var (a, b) in es)
+			{
+				if (!uf.Union(a, b)) return false;
+				if (++degs[a] > 2) return false;
+				if (++degs[b] > 2) return false;
+			}
+			return true;
+		}
+	}
+}
