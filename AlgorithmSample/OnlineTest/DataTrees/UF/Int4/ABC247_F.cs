369b0391046d713c39efc4ac96817b6b11b680cb
sakapon <git@saka-pon.net>
2024-11-07 11:34:49 +0900

    Update test

--- AlgorithmSample/OnlineTest/DataTrees/UF/Int4/ABC247_F.cs
+++ AlgorithmSample/OnlineTest/DataTrees/UF/Int4/ABC247_F.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using AlgorithmLab.DataTrees.UF401;
+using AlgorithmLab.DataTrees.UF411;
 
 namespace OnlineTest.DataTrees.UF
 {
@@ -21,13 +21,21 @@ namespace OnlineTest.DataTrees.UF
 			for (int i = 0; i < n; i++)
 				uf.Union(p[i], q[i]);
 
+			// 各連結成分に含まれるカードは、{ (1, 2), (2, 3), (3, 1) } のような形式。
+			// 選択したカードの番号が覆う区間の長さを考えると、「n」「n - 1 (左端を選択しない)」「n - 1 (右端を選択しない)」に分類される。
+			// e[n]: 区間の長さが n である場合の数 とすると、
+			// e = (1, 1, 2, 3, 5, ...) (n >= 1)
+			// e[i] = e[i - 1] + e[i - 2]
+			// f[i] = e[i] + 2 * e[i - 1]
+			// f もまたリュカ数列となる。
+
 			var f = new long[n + 1];
 			f[0] = 2;
 			f[1] = 1;
 			for (int i = 2; i <= n; i++)
 				f[i] = (f[i - 1] + f[i - 2]) % M;
 
-			return uf.ToSets().Select(g => f[g.Count()]).Aggregate((x, y) => x * y % M);
+			return uf.GetSetInfoes().Select(g => f[g.Size]).Aggregate((x, y) => x * y % M);
 		}
 	}
 }
